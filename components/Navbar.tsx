6f0b4121e1d48074e745a4bce66c6a58c8397881
Arsenios Hadjikyriacou <a.hadjikyriacou@deltasoftsolutions.net>
2026-01-23 17:48:51 +0200

    Testimonials link back to navbar and footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const navLinks = [
   { href: '#about', label: 'About' },
   { href: '#projects', label: 'Projects' },
   { href: '#experience', label: 'Experience' },
-  // { href: '#testimonials', label: 'Testimonials' },
+  { href: '#testimonials', label: 'Testimonials' },
   { href: '#contact', label: 'Contact' },
 ];
 
